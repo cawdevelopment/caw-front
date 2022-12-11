a35cbb1c98391bdf7b49718846595f86102e9bf2
draos2q <draos2q.caw@proton.me>
2022-12-11 18:36:57 +0100

    refactor : moving pages into their proper folders

---
+++ src/pages/auth/mint/index.tsx
@@ -0,0 +1,22 @@
+import { Container } from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
+
+import PageWrapper, { Layout } from 'src/components/wrappers/Page';
+import { FormStepper } from "./FormStepper";
+
+RegisterPage.getLayout = function getLayout(page: React.ReactElement) {
+    return <Layout variant="logoOnly">{page}</Layout>;
+};
+
+export default function RegisterPage() {
+
+    const { t } = useTranslation();
+
+    return (
+        <PageWrapper title={t('minting_page.title')}>
+            <Container w="full" maxW={"container.xl"} h="container.lg" p={0}>
+                <FormStepper />
+            </Container>
+        </PageWrapper>
+    );
+}
